2050bf6e930822f463ed9f3851ca4381f3bb147b
Oleksii <71426795+oleksii-molozhanov@users.noreply.github.com>
2025-04-05 22:35:34 +0200

    Clear info panel after construction

--- frontend/src/js/scenes/GameScene.js
+++ frontend/src/js/scenes/GameScene.js
@@ -195,6 +195,7 @@ export default class GameScene extends Phaser.Scene {
         
         // Update UI
         if (this.uiScene) {
+            this.uiScene.clearInfoPanel();
             this.uiScene.refreshUI();
         }
     }
