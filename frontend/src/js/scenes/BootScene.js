fa1f286ea3c33c2a435693cd162dfe86a94e28c4
Oleksii <71426795+oleksii-molozhanov@users.noreply.github.com>
2025-04-10 22:40:39 +0200

    Add event cards (green) texture

--- frontend/src/js/scenes/BootScene.js
+++ frontend/src/js/scenes/BootScene.js
@@ -58,6 +58,7 @@ export default class BootScene extends Phaser.Scene {
         // Load card background
         this.load.image('cardBackground', require('../../assets/images/ui_pack_space/Blue/Default/button_square_header_large_rectangle.png'));
         this.load.image('cardPrefabBackground', require('../../assets/images/ui_pack_space/Yellow/Default/button_square_header_large_rectangle.png'));
+        this.load.image('cardEventBackground', require('../../assets/images/ui_pack_space/Green/Default/button_square_header_large_rectangle.png'));
 
         
         // Load card slot background
