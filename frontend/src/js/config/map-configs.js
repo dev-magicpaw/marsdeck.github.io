5a785d289d9bed2b1b3d2ab3203377558ddd3195
Oleksii <71426795+oleksii-molozhanov@users.noreply.github.com>
2025-04-13 13:49:54 +0200

    Adjust tutorial map

--- frontend/src/js/config/map-configs.js
+++ frontend/src/js/config/map-configs.js
@@ -1,7 +1,7 @@
 import { TERRAIN_FEATURES } from './game-data';
 
 // Sample map configuration with specific terrain features
-export const SAMPLE_MAP = {
+export const TUTORIAL_MAP = {
     gridSize: 8, // Size of the map grid (8x8)
     cells: [
         // Metal deposits in specific locations
@@ -140,44 +140,3 @@ export const LEVEL_4_MAP = {
         
     ]
 };
-
-// A map with lots of resources
-export const RESOURCE_RICH_MAP = {
-    gridSize: 8,
-    cells: [
-        // Metal deposits (more than usual)
-        { x: 1, y: 1, feature: TERRAIN_FEATURES.METAL.id },
-        { x: 1, y: 6, feature: TERRAIN_FEATURES.METAL.id },
-        { x: 2, y: 3, feature: TERRAIN_FEATURES.METAL.id },
-        { x: 3, y: 5, feature: TERRAIN_FEATURES.METAL.id },
-        { x: 5, y: 1, feature: TERRAIN_FEATURES.METAL.id },
-        { x: 6, y: 6, feature: TERRAIN_FEATURES.METAL.id },
-        
-        // Water deposits (more than usual)
-        { x: 3, y: 2, feature: TERRAIN_FEATURES.WATER.id },
-        { x: 4, y: 5, feature: TERRAIN_FEATURES.WATER.id },
-        { x: 2, y: 6, feature: TERRAIN_FEATURES.WATER.id },
-        { x: 6, y: 2, feature: TERRAIN_FEATURES.WATER.id },
-        
-        // Just a few mountains
-        { x: 7, y: 7, feature: TERRAIN_FEATURES.MOUNTAIN.id },
-        { x: 0, y: 0, feature: TERRAIN_FEATURES.MOUNTAIN.id }
-    ]
-};
-
-// Map with buildings pre-placed
-export const TUTORIAL_MAP = {
-    gridSize: 8,
-    cells: [
-        // Resources
-        { x: 1, y: 1, feature: TERRAIN_FEATURES.METAL.id },
-        { x: 6, y: 6, feature: TERRAIN_FEATURES.METAL.id },
-        { x: 3, y: 2, feature: TERRAIN_FEATURES.WATER.id },
-        { x: 4, y: 5, feature: TERRAIN_FEATURES.WATER.id },
-        
-        // Pre-placed buildings (just examples - these aren't actual building IDs)
-        // You'll need to use the correct building IDs from your BUILDINGS object
-        // { x: 2, y: 2, building: 'droneDepo' },
-        // { x: 5, y: 5, building: 'windTurbine' }
-    ]
-}; 
