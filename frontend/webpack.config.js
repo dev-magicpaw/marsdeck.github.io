f871822517d203618a87989845ee7f01b060e820
Oleksii <71426795+oleksii-molozhanov@users.noreply.github.com>
2025-04-20 00:12:28 +0200

    Prepare to host on github

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,12 +1,35 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/js/main.js',
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '',
+        clean: true
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+        splitChunks: {
+            chunks: 'all',
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                },
+                phaser: {
+                    test: /[\\/]node_modules[\\/]phaser[\\/]/,
+                    name: 'phaser',
+                    chunks: 'all'
+                }
+            }
+        }
     },
     devServer: {
         static: './dist',
@@ -20,7 +43,10 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif|ogg|mp3|wav)$/i,
-                type: 'asset/resource'
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/[hash][ext][query]'
+                }
             }
         ]
     },
@@ -28,6 +54,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/assets/.nojekyll', to: '' }
+            ]
         })
     ]
 }; 
